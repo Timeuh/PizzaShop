91254db3b678592af4a0250cb1e089a92ffafa30
Timeuh <timeuuh@gmail.com>
2023-09-12 17:48:53 +0200

    feat: ajoute une méthode pour convertir en DTO

--- shop.pizza-shop/src/domain/entities/commande/Commande.php
+++ shop.pizza-shop/src/domain/entities/commande/Commande.php
@@ -2,19 +2,30 @@
 
 namespace pizzashop\shop\domain\entities\commande;
 
-class Commande extends \Illuminate\Database\Eloquent\Model
-{
+use pizzashop\shop\domain\dto\commande\CommandeDTO;
 
+class Commande extends \Illuminate\Database\Eloquent\Model {
     protected $connection = 'commande';
     protected $table = 'commande';
     protected $primaryKey = 'id';
     protected $keyType = 'string';
     public $timestamps = false;
-    protected $fillable = [ 'type_livraison','id','delai','date_commande','etat','montant_total','id_client'];
+    protected $fillable = ['type_livraison', 'id', 'delai', 'date_commande', 'etat', 'montant_total', 'id_client'];
 
-    public function items()
-    {
+    public function items() {
         return $this->hasMany(Item::class, 'commande_id');
     }
 
+    public function toDTO(): CommandeDTO {
+        return new CommandeDTO(
+            $this->id,
+            $this->date_commande,
+            $this->type_livraison,
+            $this->etat,
+            $this->montant_total,
+            $this->id_client,
+            $this->items,
+            $this->delai
+        );
+    }
 }
