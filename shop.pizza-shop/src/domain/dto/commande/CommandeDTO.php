54a98f5241ee32d95d20d0bd815e54d3cfdd2540
Timeuh <timeuuh@gmail.com>
2023-09-12 17:07:11 +0200

    refactor: utilise une string au lieu d'UUID

--- shop.pizza-shop/src/domain/dto/commande/CommandeDTO.php
+++ shop.pizza-shop/src/domain/dto/commande/CommandeDTO.php
@@ -4,17 +4,16 @@ namespace pizzashop\shop\domain\dto\commande;
 
 use DateTime;
 use pizzashop\shop\domain\dto\DTO;
-use Ramsey\Uuid\Uuid;
 
 class CommandeDTO extends DTO {
-    public Uuid $id;
+    public string $id;
     public DateTime $date_commande;
     public int $type_livraison;
     public int $etat;
     public float $montant_total;
     public string $id_client;
 
-    public function __construct(Uuid $id, DateTime $date_commande, int $type_livraison, int $etat, float $montant_total, string $id_client) {
+    public function __construct(string $id, DateTime $date_commande, int $type_livraison, int $etat, float $montant_total, string $id_client) {
         $this->id = $id;
         $this->date_commande = $date_commande;
         $this->type_livraison = $type_livraison;
