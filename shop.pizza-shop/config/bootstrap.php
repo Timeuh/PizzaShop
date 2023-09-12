3e0e044eb2f0f92f39c3f853c46c7aab6f956d3e
Timeuh <timeuuh@gmail.com>
2023-09-12 16:48:16 +0200

    feat: crée le fichier bootstrap

---
+++ shop.pizza-shop/config/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as DB;
+use Slim\Factory\AppFactory;
+
+session_start();
+// crée l'app et le moteur de templates
+$app = AppFactory::create();
+
+// ajoute le routing et l'erreur middleware
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, false, false);
+$app->setBasePath('');
+
+// initialise Eloquent avec les fichiers de config
+$db = new DB();
+$db->addConnection(parse_ini_file('catalog.db.ini'), 'catalog');
+$db->addConnection(parse_ini_file('commande.db.ini'), 'commande');
+$db->setAsGlobal();
+$db->bootEloquent();
+
+return $app;
