05ef1fd34fb4a15eaae0252637bd628c15b21454
Nathan <nathan.melbeck@gmail.com>
2023-09-19 16:04:57 +0200

    feat: méthode de service catalogue

--- shop.pizza-shop/src/domain/service/catalogue/IInfoProduit.php
+++ shop.pizza-shop/src/domain/service/catalogue/IInfoProduit.php
@@ -2,13 +2,22 @@
 
 namespace pizzashop\shop\domain\service\catalogue;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use pizzashop\shop\domain\dto\catalogue\ProduitDTO;
+use pizzashop\shop\domain\entities\catalogue\Produit;
+use pizzashop\shop\domain\exception\ProduitNonTrouveeException;
 use pizzashop\shop\domain\service\catalogue\ServiceCatalogue;
 
 class IInfoProduit implements ServiceCatalogue {
 
     public function getProduit(int $num, int $taille): ProduitDTO {
-        //TODO: Implement getProduit() method.
+        try{
+            $produit = Produit::where('id', $num)->andWhere('taille', $taille)->firstOrFail();
+        } catch (ModelNotFoundException $e) {
+            throw new ProduitNonTrouveeException($num);
+        }
+        return new ProduitDTO($produit->id, $produit->nom, $produit->description, $produit->prix, $produit->taille);
+
     }
 
 }
--- shop.pizza-shop/src/domain/service/catalogue/ServiceCatalogue.php
+++ shop.pizza-shop/src/domain/service/catalogue/ServiceCatalogue.php
@@ -4,4 +4,8 @@ namespace pizzashop\shop\domain\service\catalogue;
 
 interface ServiceCatalogue {
 
+    public function getProduit(int $num, int $taille): ProduitDTO;
+    public function getProduitsParCategorie($categorie): array;
+    public function getAllProduct(): array;
+
 }
