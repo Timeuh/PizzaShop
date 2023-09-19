d54d778ccd85fe53bda47a2f3bb8e3844afee7a3
Sonii79 <sonij590833@gmail.com>
2023-09-19 16:08:04 +0200

    feat: creerCommande

---
+++ shop.pizza-shop/src/domain/exception/ServiceCommandeInvalideDonneeException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace pizzashop\shop\domain\exception;
+
+class ServiceCommandeInvalideDonneeException extends \Exception {
+    public function __construct() {
+        parent::__construct("Probleme au niveau des items );
+    }
+}
