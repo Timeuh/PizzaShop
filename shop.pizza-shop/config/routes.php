6ea899812087e437124540ab36d662353519c6c7
Timeuh <timeuuh@gmail.com>
2023-09-26 17:25:29 +0200

    feat: optimise les imports

--- shop.pizza-shop/config/routes.php
+++ shop.pizza-shop/config/routes.php
@@ -1,14 +1,14 @@
 <?php
 declare(strict_types=1);
 
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
+use pizzashop\shop\app\actions\AccederCommandeAction;
+use pizzashop\shop\app\actions\CreerCommandeAction;
 
 return function( \Slim\App $app):void {
 
-    $app->post('/commandes[/]', \pizzashop\shop\app\actions\CreerCommandeAction::class)
+    $app->post('/commandes[/]', CreerCommandeAction::class)
         ->setName('creer_commande');
 
-    $app->get('/commandes/{id_commande}[/]', \pizzashop\shop\app\actions\AccederCommandeAction::class)
+    $app->get('/commandes/{id_commande}[/]', AccederCommandeAction::class)
         ->setName('commande');
 };
