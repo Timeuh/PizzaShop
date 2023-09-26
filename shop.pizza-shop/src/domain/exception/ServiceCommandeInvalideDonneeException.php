32666d39f43c6313dd7a2e077d96d25942206f74
Timeuh <timeuuh@gmail.com>
2023-09-26 14:33:15 +0200

    fix: ajoute la quote manquante dans l'exception

--- shop.pizza-shop/src/domain/exception/ServiceCommandeInvalideDonneeException.php
+++ shop.pizza-shop/src/domain/exception/ServiceCommandeInvalideDonneeException.php
@@ -4,6 +4,6 @@ namespace pizzashop\shop\domain\exception;
 
 class ServiceCommandeInvalideDonneeException extends \Exception {
     public function __construct() {
-        parent::__construct("Probleme au niveau des items );
+        parent::__construct("Probleme au niveau des items");
     }
 }
