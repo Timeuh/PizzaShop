4ee76447e2df705384271cad98814d1945a3bbb5
NathanMelbeck <94992898+NathanMelbeck@users.noreply.github.com>
2023-09-26 16:59:55 +0200

    Api/get commande (#7)
    * feat: API route + action AccederCommandeAction
    
    * refractor: action AccederCommandeAction
    
    * refractor: service catalogue
    
    * refractor: service commande

--- shop.pizza-shop/src/domain/service/catalogue/IInfoProduit.php
+++ shop.pizza-shop/src/domain/service/catalogue/IInfoProduit.php
@@ -8,16 +8,8 @@ use pizzashop\shop\domain\entities\catalogue\Produit;
 use pizzashop\shop\domain\exception\ProduitNonTrouveeException;
 use pizzashop\shop\domain\service\catalogue\ServiceCatalogue;
 
-class IInfoProduit implements ServiceCatalogue {
+interface IInfoProduit {
 
-    public function getProduit(int $num, int $taille): ProduitDTO {
-        try{
-            $produit = Produit::where('id', $num)->andWhere('taille', $taille)->firstOrFail();
-        } catch (ModelNotFoundException $e) {
-            throw new ProduitNonTrouveeException($num);
-        }
-        return new ProduitDTO($produit->id, $produit->nom, $produit->description, $produit->prix, $produit->taille);
-
-    }
+    public function getProduit(int $num, int $taille): ProduitDTO;
 
 }
