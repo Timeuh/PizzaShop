aa2343de88c741cac3ae006f5f74ef6ffe9697fb
Timothée Brindejonc <96261549+Timeuh@users.noreply.github.com>
2023-09-26 17:26:35 +0200

    get config (#8)
    * Validation donnee commande (#6)
    
    * feat: creerCommande
    
    * feat: creerCommande
    
    * feat: creerCommande
    
    * feat: Validation et exception commande
    
    * refactor: ValidationCommandeException.php
    
    * Api/get commande (#7)
    
    * feat: API route + action AccederCommandeAction
    
    * refractor: action AccederCommandeAction
    
    * refractor: service catalogue
    
    * refractor: service commande
    
    * feat: ajoute le middleware de parsing
    
    ---------
    
    Co-authored-by: Soni <97917784+Sonii08@users.noreply.github.com>
    Co-authored-by: NathanMelbeck <94992898+NathanMelbeck@users.noreply.github.com>

--- shop.pizza-shop/src/domain/service/catalogue/IBrowserCatalogue.php
+++ shop.pizza-shop/src/domain/service/catalogue/IBrowserCatalogue.php
@@ -9,44 +9,13 @@ use pizzashop\shop\domain\entities\catalogue\Produit;
 use pizzashop\shop\domain\exception\ProduitNonTrouveeException;
 use pizzashop\shop\domain\service\catalogue\ServiceCatalogue;
 
-class IBrowserCatalogue implements ServiceCatalogue {
+interface IBrowserCatalogue {
 
-    public function getProduit(int $num, int $taille): ProduitDTO {
-        try{
-            $produit = Produit::where('id', $num)->andWhere('taille', $taille)->firstOrFail();
-        } catch (ModelNotFoundException $e) {
-            throw new ProduitNonTrouveeException($num);
-        }
-        return new ProduitDTO($produit->id, $produit->nom, $produit->description, $produit->prix, $produit->taille);
+    public function getProduit(int $num, int $taille): ProduitDTO;
+    public function getProduitsParCategorie($categorie): array;
+    public function getAllProduct(): array;
 
-    }
 
-    public function getProduitsParCategorie($categorie): array {
-        try {
-            $produits = Produit::where('categorie', $categorie)->get();
-        } catch (ModelNotFoundException $e) {
-            throw new Exception("Aucun produit trouvé");
-        }
-        $produitsDTO = [];
-        foreach ($produits as $produit) {
-            $produitsDTO[] = new ProduitDTO($produit->id, $produit->nom, $produit->description, $produit->prix, $produit->taille);
-        }
-        return $produitsDTO;
-
-    }
-
-    public function getAllProduct(): array {
-        try {
-            $produits = Produit::all();
-        } catch (ModelNotFoundException $e) {
-            throw new Exception("Aucun produit trouvé");
-        }
-        $produitsDTO = [];
-        foreach ($produits as $produit) {
-            $produitsDTO[] = new ProduitDTO($produit->id, $produit->nom, $produit->description, $produit->prix, $produit->taille);
-        }
-        return $produitsDTO;
-    }
 
 
 
--- shop.pizza-shop/src/domain/service/catalogue/ServiceCatalogue.php
+++ shop.pizza-shop/src/domain/service/catalogue/ServiceCatalogue.php
@@ -1,11 +1,47 @@
 <?php
 
 namespace pizzashop\shop\domain\service\catalogue;
+use pizzashop\shop\domain\service\catalogue\IInfoProduit;
+use pizzashop\shop\domain\service\catalogue\IBrowserCatalogue;
+use pizzashop\shop\domain\dto\catalogue\ProduitDTO;
 
-interface ServiceCatalogue {
 
-    public function getProduit(int $num, int $taille): ProduitDTO;
-    public function getProduitsParCategorie($categorie): array;
-    public function getAllProduct(): array;
+class ServiceCatalogue implements IInfoProduit, IBrowserCatalogue{
+
+public function getProduit(int $num, int $taille): ProduitDTO {
+    try{
+        $produit = Produit::where('id', $num)->andWhere('taille', $taille)->firstOrFail();
+    } catch (ModelNotFoundException $e) {
+        throw new ProduitNonTrouveeException($num);
+    }
+    return new ProduitDTO($produit->id, $produit->nom, $produit->description, $produit->prix, $produit->taille);
+
+}
+    public function getProduitsParCategorie($categorie): array {
+        try {
+            $produits = Produit::where('categorie', $categorie)->get();
+        } catch (ModelNotFoundException $e) {
+            throw new Exception("Aucun produit trouvé");
+        }
+        $produitsDTO = [];
+        foreach ($produits as $produit) {
+            $produitsDTO[] = new ProduitDTO($produit->id, $produit->nom, $produit->description, $produit->prix, $produit->taille);
+        }
+        return $produitsDTO;
+
+    }
+
+    public function getAllProduct(): array {
+        try {
+            $produits = Produit::all();
+        } catch (ModelNotFoundException $e) {
+            throw new Exception("Aucun produit trouvé");
+        }
+        $produitsDTO = [];
+        foreach ($produits as $produit) {
+            $produitsDTO[] = new ProduitDTO($produit->id, $produit->nom, $produit->description, $produit->prix, $produit->taille);
+        }
+        return $produitsDTO;
+    }
 
 }
