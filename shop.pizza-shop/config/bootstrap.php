8676c5ffe3ec4ea30a5b518ac17e23d16c4adb3a
Sonii79 <sonij590833@gmail.com>
2023-09-28 13:19:41 +0200

    refactor: réorganisation du container dans bootstrap, déplacer dans dependencies.php

--- shop.pizza-shop/config/bootstrap.php
+++ shop.pizza-shop/config/bootstrap.php
@@ -1,35 +1,15 @@
 <?php
 
-use DI\ContainerBuilder;
 use Illuminate\Database\Capsule\Manager as DB;
-use Monolog\Logger;
-use pizzashop\shop\domain\service\commande\ServiceCommande;
-use Psr\Container\ContainerInterface;
 use Slim\Factory\AppFactory;
 
 session_start();
 // crée l'app et le moteur de templates
 
-$builder = new ContainerBuilder();
-$c = $builder->addDefinitions([
-
-    'log.name' => 'commande.log',
-    'log.file' => __DIR__.'/../src/console/commande.log',
-    'log.level' => Logger::WARNING,
-
-    'logger' => function (ContainerInterface $c) {
-        $log = new Logger($c->get('log.name'));
-        $log->pushHandler(new \Monolog\Handler\StreamHandler($c->get('log.file')));
-        return $log;
-    },
-
-    'commande.service' => function (ContainerInterface $c) {
-        return new ServiceCommande($c->get('logger'));
-    },
-])->build();
 
 
-$app = AppFactory::createFromContainer($c);
+$container = require __DIR__ . '/dependencies.php';
+$app = AppFactory::createFromContainer($container);
 $container = $app->getContainer();
 
 // ajoute le routing et l'erreur middleware
