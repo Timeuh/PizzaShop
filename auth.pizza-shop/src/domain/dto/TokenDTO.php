6fc0b0f634a5ef74e7cdef6aa08cf6c01d6d280a
Timeuh <timeuuh@gmail.com>
2023-10-10 17:30:38 +0200

    feat: complète le DTO

--- auth.pizza-shop/src/domain/dto/TokenDTO.php
+++ auth.pizza-shop/src/domain/dto/TokenDTO.php
@@ -2,6 +2,18 @@
 
 namespace pizzashop\auth\api\domain\dto;
 
-class TokenDTO extends DTO {
+use Firebase\JWT\JWT;
 
+class TokenDTO extends DTO {
+    public string $refreshToken;
+    public JWT $jwt;
+
+    /**
+     * @param string $refreshToken
+     * @param JWT $jwt
+     */
+    public function __construct(string $refreshToken = '', JWT $jwt = null) {
+        $this->refreshToken = $refreshToken;
+        $this->jwt = $jwt;
+    }
 }
