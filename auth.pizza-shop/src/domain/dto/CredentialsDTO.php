28baa68d809e04f849c5b49626d67977edcfd0e2
hxsm <romain.alexandre5@orange.fr>
2023-10-10 17:04:31 +0200

    methode register, activate dans AuthProvider.php

--- auth.pizza-shop/src/domain/dto/CredentialsDTO.php
+++ auth.pizza-shop/src/domain/dto/CredentialsDTO.php
@@ -4,4 +4,16 @@ namespace pizzashop\auth\api\domain\dto;
 
 class CredentialsDTO extends DTO {
 
+    public string $email;
+    public string $password;
+
+    /**
+     * @param string $email
+     * @param string $password
+     */
+    public function __construct(string $email, string $password)
+    {
+        $this->email = $email;
+        $this->password = $password;
+    }
 }
