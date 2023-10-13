a251ecea390b10975440c3cc83ea60933163506a
Timeuh <timeuuh@gmail.com>
2023-10-13 10:01:01 +0200

    feat: avec dotenv, lit la variable JWT_SECRET et l'écrit si elle n'existe pas

--- auth.pizza-shop/config/bootstrap.php
+++ auth.pizza-shop/config/bootstrap.php
@@ -7,6 +7,14 @@ use Slim\Factory\AppFactory;
 session_start();
 $app = AppFactory::create();
 
+// initialise le chemin vers le .env et le nom du fichier
+$envFileDir = __DIR__.'/../../../config';
+$envFilePath = $envFileDir.'/.env';
+
+// initialise dotenv pour accéder au .env partout dans l'app
+$dotenv = Dotenv\Dotenv::createImmutable($envFileDir);
+$dotenv->load();
+
 $app->addBodyParsingMiddleware();
 $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, false, false);
@@ -18,13 +26,13 @@ $db->addConnection(parse_ini_file('auth.db.ini'), 'auth');
 $db->setAsGlobal();
 $db->bootEloquent();
 
-if  (!getenv('JWT_SECRET')) {
+if  (!$_ENV['JWT_SECRET']) {
     try {
         $secret = bin2hex(random_bytes(32));
-    } catch (\Exception $e) {
+    } catch (Exception $e) {
         throw new JwtSecretEcritureException();
     }
-    file_put_contents('auth.env', 'JWT_SECRET=' . $secret . PHP_EOL, FILE_APPEND);
+    file_put_contents($envFilePath, 'JWT_SECRET=' . $secret . PHP_EOL, FILE_APPEND);
 }
 
 return $app;
