47536b0d05169006ae980be1476bf010af95c335
Timeuh <timeuuh@gmail.com>
2023-10-13 11:26:44 +0200

    feat: complète la méthode de refresh à l'aide d'un refresh token

--- auth.pizza-shop/src/domain/service/AuthService.php
+++ auth.pizza-shop/src/domain/service/AuthService.php
@@ -2,11 +2,24 @@
 
 namespace pizzashop\auth\api\domain\service;
 
+use DateTime;
+use Exception;
+use pizzashop\auth\api\app\auth\managers\JwtManager;
+use pizzashop\auth\api\app\auth\providers\AuthProvider;
+use pizzashop\auth\api\app\domain\entities\Users;
 use pizzashop\auth\api\domain\dto\CredentialsDTO;
 use pizzashop\auth\api\domain\dto\TokenDTO;
 use pizzashop\auth\api\domain\dto\UserDTO;
+use pizzashop\auth\api\domain\exception\RefreshUtilisateurException;
 
 class AuthService implements AuthServiceInterface {
+    private JwtManager $jwtManager;
+    private AuthProvider $authProvider;
+
+    public function __construct(JwtManager $jwtManager, AuthProvider $authProvider) {
+        $this->jwtManager = $jwtManager;
+        $this->authProvider = $authProvider;
+    }
 
     /**
      * @inheritDoc
@@ -33,7 +46,23 @@ class AuthService implements AuthServiceInterface {
      * @inheritDoc
      */
     public function refresh(TokenDTO $tokenDTO): TokenDTO {
-        // TODO: Implement refresh() method.
+        $this->authProvider->checkToken($tokenDTO->refreshToken);
+        try {
+            $user = Users::where('refresh_token', $tokenDTO->refreshToken)->firstOrFail();
+
+            $newRefreshToken = bin2hex(random_bytes(32));
+            $now = new DateTime();
+            $refreshTokenExpDate = $now->modify('+1 hour');
+
+            $user->refresh_token = $newRefreshToken;
+            $user->refresh_token_expiration_date = $refreshTokenExpDate->format('Y-m-d H:i:s');
+            $user->save();
+
+            $token = $this->jwtManager->create(['username' => $user->username, 'email' => $user->email]);
+            return new TokenDTO($newRefreshToken, $token);
+        } catch (Exception $e) {
+            throw new RefreshUtilisateurException();
+        }
     }
 
     /**
