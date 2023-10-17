f510e89243ed997d963b20f3a946ec73806acb36
Timeuh <timeuuh@gmail.com>
2023-10-17 16:40:00 +0200

    feat: ajoute la route pour se connecter avec un refresh token

--- auth.pizza-shop/config/routes.php
+++ auth.pizza-shop/config/routes.php
@@ -1,5 +1,8 @@
 <?php
 declare(strict_types=1);
 
-return function( \Slim\App $app):void {
+use pizzashop\auth\api\app\actions\UserRefreshAction;
+
+return function(\Slim\App $app):void {
+    $app->post('/api/users/refresh', UserRefreshAction::class)->setName('refreshUser');
 };
