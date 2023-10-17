af44c888448be0f98e03c53acdd1f7ffd62e9ecb
Timeuh <timeuuh@gmail.com>
2023-10-17 15:38:00 +0200

    feat: ajoute l'interface action

---
+++ auth.pizza-shop/src/app/actions/AbstractAction.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace pizzashop\auth\api\app\actions;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+abstract class AbstractAction {
+    public abstract function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface;
+}
