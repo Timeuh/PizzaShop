c3b6596d217c80599b7e336fc696536645637f94
Timeuh <timeuuh@gmail.com>
2023-10-17 15:42:23 +0200

    feat: ajoute le service d'authentification au constructeur

--- auth.pizza-shop/src/app/actions/AbstractAction.php
+++ auth.pizza-shop/src/app/actions/AbstractAction.php
@@ -2,9 +2,16 @@
 
 namespace pizzashop\auth\api\app\actions;
 
+use pizzashop\auth\api\domain\service\AuthService;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 abstract class AbstractAction {
+    private AuthService $authService;
+
+    public function __construct(AuthService $authService) {
+        $this->authService = $authService;
+    }
+
     public abstract function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface;
 }
