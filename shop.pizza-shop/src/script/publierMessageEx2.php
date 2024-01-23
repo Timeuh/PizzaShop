07a03e7d0b49adffe11200168152fd7748d98b59
NathanMelbeck <94992898+NathanMelbeck@users.noreply.github.com>
2024-01-23 16:36:36 +0100

    Rabbitmq/commande (#31)
    * feat: ajout php amqplib au composer
    
    * :wrench: config: installe amqp-lib
    
    * feat: script publierMessage.php
    
    * :bug: fix:
    
    * :loud_sound: feat: log la commande créée
    
    * feat: script consumeMessage.php
    
    * fix: refractor name script consume et publier
    
    * feat: souscritConsumeEx3.php enregistre un callback utilisé pour les messages
    
    * feat: ajout de msg sur rabbitmq lors de la validation d'une commande
    
    * config: composer.lock
    
    ---------
    
    Co-authored-by: Timeuh <timeuuh@gmail.com>

---
+++ shop.pizza-shop/src/script/publierMessageEx2.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__ . './../../vendor/autoload.php';
+
+use PhpAmqpLib\Message\AMQPMessage;
+
+
+
+$message_queue = 'nouvelles_commandes';
+$connection = new \PhpAmqpLib\Connection\AMQPStreamConnection('rabbitmq', 5672, 'rabbitUser', 'rabbitPass');
+$channel = $connection->channel();
+$msg_body = [
+    'id' => 1,
+    'nom' => 'Pizza 4 fromages',
+    'prix' => 12.5,
+    'ingredients' => [
+        'Mozzarella',
+        'Gorgonzola',
+        'Chèvre',
+        'Parmesan'
+]];
+$channel->basic_publish(new AMQPMessage(json_encode($msg_body)), 'pizzashop', 'nouvelle');
+print "[x] commande publiée : \n";
+print json_encode($msg_body);
+$channel->close();
+$connection->close();
