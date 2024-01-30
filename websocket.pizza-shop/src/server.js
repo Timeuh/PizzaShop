428ec44cf9edb9c0dbedf4e5f2bae724aa681398
Nathan <nathan.melbeck@gmail.com>
2024-01-30 14:16:41 +0100

    feat: server.js (webSocket)

--- websocket.pizza-shop/src/server.js
+++ websocket.pizza-shop/src/server.js
@@ -1,12 +1,19 @@
-import http from 'node:http';
-
-const reqHandler = async (req, res) => {
-  res.writeHead(200);
-  res.end({
-    response: "success"
+import WebSocket, { WebSocketServer } from 'ws';
+const server= new WebSocketServer({ port: 3000 , clientTracking: true});
+const notifyAll = (msg) =>{
+  server.clients.forEach((client_socket) => {
+    if (client_socket.readyState === WebSocket.OPEN)
+      client_socket.send(msg);
   });
 }
-const server = http.createServer(reqHandler);
-server.listen(3000, 'localhost', () => {
-  console.log('node: http://localhost:3000');
-});
+server.addEventListener('connection', (client_socket) => {
+  client_socket.addEventListener('error', console.error);
+  client_socket.addEventListener('message', (message) => {
+    console.log('received: %s', message);
+    client_socket.send('acknowledge msg : ' + message);
+    notifyAll(message);
+  });
+  client_socket.addEventListener('close', (event) => {
+    console.log('client disconnected ');
+  });
+})
