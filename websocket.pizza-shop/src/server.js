1c7126540431bd78c520a253b8db3e653c4327bf
Timeuh <timeuuh@gmail.com>
2024-02-04 18:03:03 +0100

    feat: :sparkles: utilise le service amqp pour notifier un client quand sa commande change

--- websocket.pizza-shop/src/server.js
+++ websocket.pizza-shop/src/server.js
@@ -1,5 +1,9 @@
 import { WebSocketServer } from 'ws';
+import ConsumeService from "./services/ConsumeService.js";
+
 const server = new WebSocketServer({ port: 3000 , clientTracking: true});
+const consumeService = new ConsumeService();
+
 const notifyAll = (msg) =>{
   server.clients.forEach((client_socket) => {
     if (client_socket.readyState === WebSocket.OPEN)
@@ -15,5 +19,20 @@ server.on('connection', (client_socket) => {
         client_socket.send('Vous êtes abonné au suivi de la commande numéro : ' + message.data );
         }
       )
-  });
+});
 
+// consomme les messages de mise à jour commande
+consumeService.consumeMessages((error, nouvelleCommande) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  // recherche le client avec le bon numero de commande
+  const clientCommande = clients.find(client => client.numCommande === nouvelleCommande.numCommande);
+
+  // notifie le client
+  if (clientCommande.clientSocket.readyState === WebSocket.OPEN) {
+    clientCommande.clientSocket.send('La commande ' + nouvelleCommande.numCommande + ' a été mise à jour : ' + JSON.stringify(nouvelleCommande));
+  }
+});
