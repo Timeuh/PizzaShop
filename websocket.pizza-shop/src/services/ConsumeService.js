8f85c30ce319e14727476117b33565e9807d671c
Timeuh <timeuuh@gmail.com>
2024-02-04 17:56:13 +0100

    feat: :sparkles: crée le service qui consomme la file amqp

---
+++ websocket.pizza-shop/src/services/ConsumeService.js
@@ -0,0 +1,35 @@
+const amqp = require('amqplib');
+
+class ConsumeService {
+
+    constructor() {
+        this.rabbitmq = 'amqp://rabbitUser:rabbitPass@rabbitmq:5672';
+        this.queue = 'suivi_commandes';
+    }
+
+    async consumeMessages(callback) {
+        try {
+            const conn = await amqp.connect(this.rabbitmq);
+            const channel = await conn.createChannel();
+            await channel.assertQueue(this.queue, {durable: true});
+
+            channel.consume(this.queue, async (msg) => {
+                try {
+                    console.log('Nouvel état commande:', msg.content.toString());
+                    const nouvelleCommande = JSON.parse(msg.content.toString());
+                    channel.ack(msg);
+                    callback(null, nouvelleCommande);
+                } catch (error) {
+                    console.error('Erreur lors du traitement du message:', error);
+                    channel.nack(msg, false, false);
+                    callback(error, null);
+                }
+            });
+        } catch (error) {
+            console.error('Erreur lors de la connexion a RabbitMQ:', error);
+            callback(error, null);
+        }
+    }
+}
+
+module.exports = ConsumeService;
